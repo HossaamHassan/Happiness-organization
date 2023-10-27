7986f705743c3f2e8a6b9c113b238ccb454f9a77
HossaamHassan <hossam.h.eldeeb@gmail.com>
2023-10-27 06:59:53 +0200

    Upload Web Site

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+// Contact Section
+let contactForm = document.querySelector(".contact-form");
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let subject = document.getElementById("subject");
+let message = document.getElementById("message");
+let send = document.getElementById("send-btn");
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value,
+  };
+  send.value = "Sending Message...";
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.onload = () => {
+    console.log(xhr.responseText);
+    if (xhr.responseText == "success") {
+      name.value = "";
+      email.value = "";
+      subject.value = "";
+      message.value = "";
+      send.value = "Message Sent Successfully!";
+    } else {
+      send.value = "Something Went Wrong!";
+    }
+  };
+  xhr.send(JSON.stringify(formData));
+});
+
+// Start Scrolling To Top
+const scrolling = document.querySelector(".arrow");
+const apperArrow = document.querySelector(".appear");
+function scrollToTop(ar) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY >= document.body.offsetTop + 300) {
+      ar.classList.add("appear");
+    } else {
+      ar.classList.remove("appear");
+    }
+  });
+
+  ar.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
+scrollToTop(scrolling);
+// End Scrolling To Top
+// Start Fetch On Data Events
+fetch("https://finaldoctoridea-default-rtdb.firebaseio.com/")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+// End Fetch On Data Events
